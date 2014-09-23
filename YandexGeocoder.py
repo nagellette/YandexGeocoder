561fb5ab31c7f39df20b7e757513ab06f6aec431
nagellette <necipenesgengec@gmail.com>
2014-09-23 23:19:42 +0300

    Build the main geocoding functions
    * Opens address files
    * Gets geocode meta info of addresses
    * Gets geocode data and meta info of geocode data

--- YandexGeocoder.py
+++ YandexGeocoder.py
@@ -1,15 +1,30 @@
-__author__ = 'Necip Enes Gengec'
 # -*- coding: utf-8 -*-
+__author__ = 'Necip Enes Gengec'
 import urllib2
 import json
+import YandexGeocoderDefs as geocoderDefs
 
-content = urllib2.urlopen("http://geocode-maps.yandex.ru/1.x/?format=json&geocode=Sultanahmet+Camii+İç+Yolları&lang=en-US")
-data = json.load(content)
+# open input address data and pass the addresses into new list
+f = open('geocode_addresses')
+addresses = f.readlines()
+f.close()
 
-data = data['response']
-data = data['GeoObjectCollection']
-data = data['metaDataProperty']
-data = data['GeocoderResponseMetaData']
-data = data['found']
+# loop on the address list and send response and get geocoded responds
+for address in addresses:
 
-print data
+    geocodeURL = 'http://geocode-maps.yandex.ru/1.x/?format=json&geocode=' + address
+    #print geocodeURL
+
+    # open response url and convert to json
+    responsecontent = urllib2.urlopen(geocodeURL)
+    jsonresponse = json.load(responsecontent)
+
+    # get geocode count for single address from meta info of geocode
+    geocodeCount = geocoderDefs.yandexMetaAnalysis(jsonresponse)
+
+    # get geocode results from json if count>0 else pass empty geocode field
+    if geocodeCount >= 1:
+        geocoderDefs.yandexGetGeocode(jsonresponse)
+        print geocoderDefs.yandexGetGeocode(jsonresponse)
+    else:
+        pass
---
+++ YandexGeocoderDefs.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Necip Enes Gengec'
+'''
+Get Yandex response meta deta
+determine response count
+'''
+def yandexMetaAnalysis(yandexjson):
+
+    analysisjson = yandexjson
+    analysisjson = analysisjson['response']
+    analysisjson = analysisjson['GeoObjectCollection']
+    analysisjson = analysisjson['metaDataProperty']
+    analysisjson = analysisjson['GeocoderResponseMetaData']
+    analysisjson = analysisjson['found']
+
+    return analysisjson
+
+'''
+get and return geocoded coordinates
+and geocode meta with a list
+'''
+def yandexGetGeocode(yandexjson):
+
+
+    analysisjson = yandexjson
+    analysisjson = analysisjson['response']
+    analysisjson = analysisjson['GeoObjectCollection']
+    analysisjson = analysisjson['featureMember']
+
+    temp1 = []
+    temp2 = []
+    temp3 = []
+    counter = 0
+    geocodedresponse = []
+
+    for feature in analysisjson:
+        #get geocode meta info
+        temp1 = feature
+        temp1 = temp1['GeoObject']
+        temp1 = temp1['metaDataProperty']
+        temp1 = temp1['GeocoderMetaData']
+        temp2.append(temp1['kind'])
+        temp2.append(temp1['precision'])
+
+        #get geocoded coordinates
+        temp1 = feature
+        temp1 = temp1['GeoObject']
+        temp1 = temp1['Point']
+        temp3 = temp1['pos'].split()
+        temp2.append(temp3[0])
+        temp2.append(temp3[1])
+        geocodedresponse.append(temp2)
+        temp1 = []
+        temp2 = []
+        temp3 = []
+        counter += 1
+
+
+    return geocodedresponse
